24d0dd6ecabd21b11f1315c246a4f0582899235f
midhun98 <midhunskani@gmail.com>
2023-10-19 16:54:53 +0530

    organize code

--- src/index.js
+++ src/index.js
@@ -1,28 +1,8 @@
 import React from "react"
 import ReactDOM from "react-dom"
 import Header from "./Header"
-
-function Footer() {
-    return (
-        <footer>
-            <small>© 2021 Ziroll development. All rights reserved.</small>
-        </footer>
-    )
-}
-
-function MainContent() {
-    return (
-        <div>
-            <h1>Reasons I'm excited to learn React</h1>
-            <ol>
-                <li>It's a popular library, so I'll be 
-                able to fit in with the cool kids!</li>
-                <li>I'm more likely to get a job as a developer
-                if I know React</li>
-            </ol>
-        </div>
-    )
-}
+import MainContent from "./MainContent"
+import Footer from "./Footer"
 
 function Page() {
     return (
